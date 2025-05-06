9e65edad73f5c76ade03b484c3f2193c5acbb83a
Antoni Załupka <antoni.zalupka@ilaresoft.com>
2025-05-06 21:13:47 +0200

    Added .env to frontend

--- frontend/src/apollo-client.ts
+++ frontend/src/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 
 export const apolloClient = new ApolloClient({
-  uri: "http://192.168.0.103:4000/", // adjust the URL to match your backend
+  uri: import.meta.env.VITE_BACKEND_URL, // adjust the URL to match your backend
   cache: new InMemoryCache(),
   credentials: "include", // if you need to handle cookies for authentication
 });
