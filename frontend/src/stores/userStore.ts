10325405cf088dc88130fbaf6906fb412fd07259
Antoni Załupka <antoni.zalupka123@gmail.com>
2025-05-25 20:38:55 +0200

    Refactor userStore: add router initialization and improve user redirection logic

--- frontend/src/stores/userStore.ts
+++ frontend/src/stores/userStore.ts
@@ -1,11 +1,14 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import { userService, type User } from "@/services/userService";
+import { useRouter } from "vue-router";
 
 export const useUserStore = defineStore("user", () => {
   const user = ref<User | null>(null);
   const isLoading = ref(false);
   const error = ref<string | null>(null);
+
+  const router = useRouter();
 
   async function login(email: string, password: string) {
     isLoading.value = true;
@@ -44,7 +47,9 @@ export const useUserStore = defineStore("user", () => {
       user.value = await userService.getMe();
       if (!user.value) {
         await userService.logout();
+        router.push("/auth/login");
       }
+      router.push("/quizzes");
       return null;
     } catch (err) {
       error.value =
